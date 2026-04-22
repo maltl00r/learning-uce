9acb055a26af7ab90e27e32a20e10c8ac0647481
Marvin Loor <76671589+maltl00r@users.noreply.github.com>
2026-04-21 21:21:44 -0500

    Implement input of even numbers into a 2D list
    Added functionality to input even numbers into a list based on user-defined rows and columns.

--- APE-PAE/pcei2a-actividad3.py
+++ APE-PAE/pcei2a-actividad3.py
@@ -1,2 +1,26 @@
 # Taller grupal para la resolución de problemas cotidianos
 # mediante el uso de listas y sus métodos en Phyton.
+
+lista=[]
+filas=int(input("ingrese numero de filas: "))
+columnas=int(input("ingrese numero de columnas:" ))
+
+for i in range(filas):
+    fila=[]
+    for j in range (columnas):
+        while True:
+            fi=int(input(f"ingrese un valor {i}{j}: "))
+            if fi % 2 == 0:
+                fila.append(fi)
+            else:
+                print("ingrese un numero par")
+              break
+    lista.append(fila)
+
+for i in lista:
+    lista.sort()
+    for j in i:
+        j.sort()
+        
+for i in lista:
+    print(i)

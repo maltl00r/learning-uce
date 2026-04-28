38f6b73088595d5f4dd99722d9f73f164931d1e4
Marvin Loor <76671589+maltl00r@users.noreply.github.com>
2026-04-28 13:03:25 +0000

    añadidura de indicaciones

---
+++ Varios/pcei2a-ejercicio_funciones.py
@@ -0,0 +1,12 @@
+# Ejercicio: Menú de operaciones matemáticas con validaciones
+#
+# Instrucciones:
+# Elaborar un programa en Python que despliegue un menú para realizar
+# las cuatro operaciones básicas. 
+#
+# Requerimientos:
+# - Implementar funciones para capturar y validar datos.
+# - Aplicar los cuatro tipos de funciones (con/sin parámetros, con/sin retorno).
+# - Validaciones: No división para 0, solo números positivos, no restas negativas.
+# - Manejo de errores para evitar caracteres (try/except).
+# - El programa permite n repeticiones.

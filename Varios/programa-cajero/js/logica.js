a685568c4b4596794ff20e71f4d82ad53592329a
Marvin Loor <76671589+maltl00r@users.noreply.github.com>
2026-05-02 01:19:15 +0000

    version 1 del cajero terminada

---
+++ Varios/programa-cajero/js/logica.js
@@ -0,0 +1,205 @@
+export const usuarios=[{cedula:"1207499999", nombres:"LOOR MARVIN", telefono:"0982799999", pin:"4321", cuentas:[
+    {
+        tipo:"AHORROS", 
+        numero:"19112005",
+        saldo:150.00
+    }]},{cedula:"1550199999", nombres:"FIALLOS BRYANA", telefono:"0991699999", pin:"8765", cuentas:[
+    {
+        tipo:"AHORROS", 
+        numero:"19052006",
+        saldo:1000.00
+    }]}];
+
+export function buscarUsuarioPorCedula(cedula) {
+    const usuario = usuarios.find(u => u.cedula === cedula);
+
+    if (!usuario) {
+        return null;
+    }
+
+    return {
+        nombres: usuario.nombres,
+        pin: usuario.pin,
+        cuentas: usuario.cuentas
+    };
+}
+
+export function validarFondos(cedula, numCuenta, monto) {
+    const usuario = buscarUsuarioPorCedula(cedula);
+    if (!usuario) return false;
+
+    const cuenta = usuario.cuentas.find(c => c.numero === numCuenta);
+
+    if (!cuenta) return false;
+    
+    return cuenta.saldo >= monto;
+
+}
+
+export function validarUsuarioExistente(cedula) {
+    return usuarios.some(u => u.cedula === cedula);
+}
+
+export function validarMonto(monto) {
+    const valor = Number(monto);
+
+    return !isNaN(valor) && valor > 0;
+}
+
+export function realizarDeposito(cedula, numCuenta, monto) {
+    const usuario = buscarUsuarioPorCedula(cedula);
+    if (!usuario) {
+        return { ok: false, mensaje: "Usuario no encontrado" };
+    }
+
+    const cuenta = usuario.cuentas.find(c => c.numero === numCuenta);
+    if (!cuenta) {
+        return { ok: false, mensaje: "Cuenta no encontrada" };
+    }
+
+    if(!validarMonto(monto)) {
+        return { ok: false, mensaje: "Monto inválido" };
+    }
+
+    cuenta.saldo += Number(monto);
+
+    return {
+        ok: true,
+        nuevoSaldo: cuenta.saldo,
+        mensaje: `Depósito de $${monto} realizado con éxito.`
+    };
+}
+
+export function realizarRetiro(cedula, numCuenta, monto) {
+    const usuario = buscarUsuarioPorCedula(cedula);
+    if (!usuario) {
+        return { ok: false, mensaje: "Usuario no encontrado" };
+    }
+
+    const cuenta = usuario.cuentas.find(c => c.numero === numCuenta);
+    if (!cuenta) {
+        return { ok: false, mensaje: "Cuenta no encontrada" };
+    }
+
+    if(!validarMonto(monto)) {
+        return { ok: false, mensaje: "Monto inválido" };
+    }
+
+    const saldo = validarFondos(cedula, numCuenta, monto)
+    if (!saldo) {
+        return { ok: false, mensaje: "Fondos insuficientes"}
+    } 
+
+    cuenta.saldo -= Number(monto);
+
+    return {
+        ok: true,
+        nuevoSaldo: cuenta.saldo,
+        mensaje: `Retiro de $${monto} realizado con éxito.`
+    } 
+}
+
+export function crearNuevoUsuario(nombre, cedula, pin) {
+    if (validarUsuarioExistente(cedula)) {
+        return { ok: false, mensaje: "El cliente ya existe" }
+    }
+    if (!nombre) {
+        return { ok: false, mensaje: "Se debe proporionar el nombre de la persona." }
+    }
+
+    if (!cedula) {
+        return { ok: false, mensaje: "Se debe proporcionar una cédula."}
+    }
+
+    if (!pin) {
+        return { ok: false, mensaje: "Se debe proporcionar un pin."}
+    }
+    
+    const nuevoUsuario = {
+        cedula,
+        nombres: nombre,
+        pin, 
+        cuentas:[]
+    };
+
+    usuarios.push(nuevoUsuario)
+    crearCuentaNueva(cedula, "AHORROS")
+
+    return { ok: true, mensaje: "Usuario creado con éxito.", usuario: nuevoUsuario };
+    
+}
+
+export function generarNumeroCuenta() {
+    const numero = Math.floor(10000000 + Math.random() * 90000000);
+    return numero.toString();
+}
+
+export function crearCuentaNueva(cedula, tipoCuenta) {
+    if (!validarUsuarioExistente(cedula)) {
+        return { ok: false, mensaje: "El cliente no existe" }
+    }
+
+    const nuevaCuenta = {
+        tipo: tipoCuenta,
+        numero: generarNumeroCuenta(),
+        saldo: 0
+    }   
+
+    let usuario = buscarUsuarioPorCedula(cedula);
+    if(!usuario) {
+        return { ok: false, mensaje: "Usuario no encontrado" }
+    }
+
+    if(!tipoCuenta) {
+        return { ok: false, mensaje: "Se debe proporcionar el tipo de cuenta"}
+    }
+
+    if (usuario.cuentas.length >= 3) {
+    return { ok: false, mensaje: "El cliente ya alcanzó el límite máximo de cuentas." };
+    }
+
+    usuario.cuentas.push(nuevaCuenta)
+
+    return { 
+        ok: true, 
+        mensaje: `Cuenta de ${tipoCuenta} creada con éxito.`,
+        cuenta: nuevaCuenta 
+    };
+}
+
+export function mostrarVista(idVista) {
+    // Escondemos todas las capas con la clase 'vista'
+    document.querySelectorAll('.vista').forEach(v => v.style.display = 'none');
+    
+    // Mostramos la que pedimos
+    document.getElementById(idVista).style.display = 'block';
+}
+
+export function validarPin(cedula, pin) {
+    if(!cedula) {
+        return { ok: false, mensaje: "Se debe proporcionar una cédula. "};
+    }
+
+    if (!pin) {
+        return { ok: false, mensaje: "Se debe proporcionar un pin. "};
+    }
+
+    if(validarUsuarioExistente(cedula)) {
+        const valor = buscarUsuarioPorCedula(cedula).pin;
+        if (valor != pin) {
+            return { ok: false, mensaje: "El pin no es correcto. "};
+        }
+
+        return { ok: true, mensaje: "El pin es correcto" }
+    }
+}
+
+export function darNumCuenta(cedula) {
+    const usuario = buscarUsuarioPorCedula(cedula);
+
+    if (!usuario || !usuario.cuentas || usuario.cuentas.length === 0) {
+        return null;
+    }
+
+    return usuario.cuentas[0].numero;
+}
---
+++ Varios/programa-cajero/js/main.js
@@ -0,0 +1,151 @@
+import { buscarUsuarioPorCedula, validarFondos, validarUsuarioExistente, validarMonto, 
+    realizarDeposito, realizarRetiro, crearNuevoUsuario, crearCuentaNueva, mostrarVista, validarPin, 
+    darNumCuenta} from './logica.js';
+
+const btnRetiro = document.getElementById('btn-retiro');
+const btnDeposito = document.getElementById('btn-deposito');
+const btnCrear = document.getElementById('btn-crear');
+const pantalla = document.getElementById('pantalla');
+const cedulaInput = `<input type="text" id="cedulaInput" placeholder="Ingrese su número de cédula">`;
+const btnBuscar = `<button id=btnBuscar>Buscar</button>`;
+
+// console.log(buscarUsuarioPorCedula("1207499999"));  [Object, object] Arroja el usuario {} completo
+
+// console.log(validarFondos("1207499999", "19112005", 150));  true, false Si tiene más de lo que intenta retirar
+
+// console.log(validarUsuarioExistente("1207499999"));  true, false Si el usuario existe o no
+
+// console.log(validarMonto(10));  true, false Permite ingresar valores válidos
+
+// console.log(realizarDeposito("1207499999", "19112005", 10)) [Object, Object] Determina si tuvo éxito o no
+
+// console.log(realizarRetiro("1207499999", "19112005", 161))  [Object, Object] Determina si tuvo éxito o no
+
+// console.log(crearNuevoUsuario("MARVINLATA", "10", "10"))  [Object, Object] Determina si tuvo éxito o no
+
+// console.log(crearCuentaNueva("1207499999", "AHORROS"))  [Object, Object] Determina si tuvo éxito o no
+const contenedor = document.getElementById('campos-dinamicos');
+const mensaje = document.getElementById('mensaje');
+
+document.getElementById('btn-cancelar').addEventListener('click', () => {
+        mostrarVista('menu-principal');
+    });
+
+document.getElementById('btn-crear').addEventListener('click', () => {
+    document.getElementById('titulo-form').innerText = "Nuevo Usuario";
+    
+    // Inyectamos el HTML de los inputs
+    contenedor.innerHTML = `
+        <input type="text" id="reg-nombre" placeholder="Nombre completo">
+        <input type="text" id="reg-cedula" placeholder="Cédula">
+        <input type="text" id="reg-pin" placeholder="PIN de 4 dígitos">
+    `;
+
+    mostrarVista('pantalla-formulario');
+
+    // Cambiamos lo que hace el botón confirmar para esta pantalla
+    document.getElementById('btn-confirmar').onclick = () => {
+        const n = document.getElementById('reg-nombre').value;
+        const c = document.getElementById('reg-cedula').value;
+        const p = document.getElementById('reg-pin').value;
+        
+        const res = crearNuevoUsuario(n, c, p);
+        mensaje.innerHTML = res.mensaje;
+        if(res.ok) {
+            mensaje.innerHTML += " Redirigiendo...";
+            setTimeout(() => {
+                mostrarVista('menu-principal');
+                mensaje.innerHTML = "";
+            }, 3000);
+        }
+    };
+});
+
+document.getElementById('btn-retiro').addEventListener('click', () => {
+    document.getElementById('titulo-form').innerText = "Retiro";
+    contenedor.innerHTML = `
+        <p>Por favor, ingrese su número de cédula: </p>
+        <input type="text" id="reg-cedula" placeholder="Cédula">
+    `;
+    
+    mostrarVista('pantalla-formulario');
+
+    document.getElementById('btn-confirmar').onclick = () => {
+        const cedula = document.getElementById('reg-cedula').value;
+        let respuesta = buscarUsuarioPorCedula(cedula);
+        if (!respuesta) {
+            mensaje.innerHTML = "El usuario no existe. ";
+            return;
+        }
+
+        document.getElementById('titulo-form').innerText = "Retiro - Pin";
+        contenedor.innerHTML = `<input type="password" id="reg-pin" placeholder="Ingrese su PIN">`;
+        mensaje.innerHTML = "";
+
+        document.getElementById('btn-confirmar').onclick = () => {
+            const pin = document.getElementById('reg-pin').value;
+            const validacion = validarPin(cedula, pin);
+            if (!validacion.ok) {
+                mensaje.innerHTML = validacion.mensaje;
+            } else {
+                mensaje.innerHTML = "";
+                document.getElementById('titulo-form').innerText = "Retiro - Cantidad";
+                contenedor.innerHTML = `<input type="number" id="reg-cantidad" placeholder="Ingrese la cantidad a retirar">`;
+
+                document.getElementById('btn-confirmar').onclick = () => {
+                    const monto = document.getElementById('reg-cantidad').value;
+                    const retiro = realizarRetiro(cedula, darNumCuenta(cedula), monto)
+                    if(!retiro.ok) {
+                        mensaje.innerHTML = retiro.mensaje;
+                    } else {
+                        mensaje.innerHTML = retiro.mensaje;
+                        mensaje.innerHTML += " Redirigiendo...";
+                        setTimeout(() => {
+                            mostrarVista('menu-principal');
+                            mensaje.innerHTML = "";
+                        }, 3000);
+                    }
+                 }
+            }
+        }
+    }
+});
+
+document.getElementById('btn-deposito').addEventListener('click', () => {
+    document.getElementById('titulo-form').innerText = "Depósito";
+    contenedor.innerHTML = `
+        <p>Por favor, ingrese su número de cuenta: </p>
+        <input type="text" id="reg-cedula" placeholder="Número de cédula">
+    `;
+
+    mostrarVista('pantalla-formulario');
+
+    document.getElementById('btn-confirmar').onclick = () => {
+
+        const cedula = document.getElementById('reg-cedula').value;
+        const respuesta = buscarUsuarioPorCedula(cedula);
+        if (!respuesta) {
+            mensaje.innerHTML = "No existe este usuario. ";
+        } else {
+            mensaje.innerHTML = "";
+            document.getElementById('titulo-form').innerText = "Deposito - Cantidad";
+            contenedor.innerHTML = `<input type="number" id="reg-cantidad" placeholder="Ingrese la cantidad a depositar">`;
+
+            document.getElementById('btn-confirmar').onclick = () => {
+                const monto = document.getElementById('reg-cantidad').value;
+                const deposito = realizarDeposito(cedula, darNumCuenta(cedula), monto);
+                if(!deposito.ok) {
+                    mensaje.innerHTML = deposito.mensaje;
+                }  else {
+                        mensaje.innerHTML = deposito.mensaje;
+                        mensaje.innerHTML += " Redirigiendo...";
+                        setTimeout(() => {
+                            mostrarVista('menu-principal');
+                            mensaje.innerHTML = "";
+                        }, 3000);
+                    }
+
+            }
+        }
+    }
+});
